6b90bd46d53d8496a99ee8fdc8b2465e583daf62
Eric Busten <eric.s.busten@gmail.com>
2025-04-14 20:43:19 -0300

    feature: change color status bar

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,15 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { theme } from './theme/global.js'
 import 'dayjs/locale/es'
 
+// Función para actualizar el theme-color
+function updateThemeColor(color: string) {
+  const metaTag = document.querySelector('meta[name="theme-color"]') || document.createElement('meta')
+  metaTag.setAttribute('name', 'theme-color')
+  metaTag.setAttribute('content', color)
+  document.head.appendChild(metaTag)
+}
+updateThemeColor(theme.palette.primary.main)
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
   <React.StrictMode>
