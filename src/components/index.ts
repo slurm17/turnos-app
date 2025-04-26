3b8411654d5d6d6ece512fa0315203c550896804
Eric Busten <eric.s.busten@gmail.com>
2025-04-26 15:30:21 -0300

    refactor: temporarily change folder names

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-export { default as PageWithOptions } from './pageWithOptions/PageWithOptions'
-export { default as Layout } from './layout/Layout'
+export { default as PageWithOptions } from './pageWithOptions_temp/PageWithOptions'
+export { default as Layout } from './layout_temp/Layout'
 export * from './reminders'
 export * from './buttons'
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,4 @@
 export { default as AddContact } from './AddContact/AddContact'
 export { default as ListContact } from './ListContact/ListContact'
-export { default as NotFound } from './notFound/NotFound'
-export { default as Reminder } from './reminder/Reminder'
+export { default as NotFound } from './notFound_temp/NotFound'
+export { default as Reminder } from './reminder_temp/Reminder'
