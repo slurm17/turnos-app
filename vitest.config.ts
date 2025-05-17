8faab4eca5d124f788eb95cc8c471dbb3c165916
Eric Busten <eric.s.busten@gmail.com>
2025-05-17 11:52:12 -0300

    refactor: change dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: './src/setupTests.ts', // Si tienes un archivo de configuración para tests
+    setupFiles: './src/setupTests.ts',
   },
 })
