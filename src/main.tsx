e5c600d6c04a7b35b5d23533b8b620e537160da7
Eric Busten <eric.s.busten@gmail.com>
2025-05-23 20:40:52 -0300

    refactor: remove unnecessary code

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.js'
-import PWABadge from './PWABadge.js'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { theme } from './theme/global.js'
 import 'dayjs/locale/es'
+import { updateThemeColor } from './utils/updateThemeColor'
 
-// Función para actualizar el theme-color
-function updateThemeColor(color: string) {
-  const metaTag = document.querySelector('meta[name="theme-color"]') || document.createElement('meta')
-  metaTag.setAttribute('name', 'theme-color')
-  metaTag.setAttribute('content', color)
-  document.head.appendChild(metaTag)
-}
 updateThemeColor(theme.palette.primary.main)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -28,7 +21,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         >
           <App />
         </LocalizationProvider>
-        <PWABadge />
     </ThemeProvider>
   </React.StrictMode>,
 )
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom/vitest' // Importación para Vitest
+
+// Para usar con MSW:
+// import { server } from './mocks/server';
+// import { beforeAll, afterEach, afterAll } from 'vitest';
+
+// beforeAll(() => server.listen());
+// afterEach(() => server.resetHandlers());
+// afterAll(() => server.close());
