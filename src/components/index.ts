3aca0706b2fc41c9b5e35bbaeffbd3b895f97721
Eric Busten <eric.s.busten@gmail.com>
2025-05-27 12:36:56 -0300

    refactor: change name folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-export { default as PageWithOptions } from './pageWithOptions/PageWithOptions'
+export { default as PageWithOptions } from './options/PageWithOptions'
 export { default as Layout } from './layout/Layout'
 export * from './reminders'
 export * from './buttons'
